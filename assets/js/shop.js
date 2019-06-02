57de4b70ff10a25c2dc832ac2e2d4f6fe49f10e7
hsiaowen0324 <47879028+hsiaowen0324@users.noreply.github.com>
2019-06-02 19:15:50 +0800

    Add files via upload

---
+++ assets/js/shop.js
@@ -0,0 +1,13 @@
+ $(function () {
+ $('#myTab a[href="#pills-all"]').tab('show')
+$('#myTab a[href="#pills-suggest"]').tab('show')
+})
+ $(function () {
+ 	
+    $('#myTab a[href="#circle"]').tab('show') 
+    $('#myTab a[href="#square"]').tab('show') 
+    $('#myTab a[href="#oval"]').tab('show') 
+    $('#myTab a[href="#rectangular"]').tab('show') 
+    $('#myTab a[href="#heart"]').tab('show') 
+    $('#myTab a[href="#triangular"]').tab('show') 
+  })
